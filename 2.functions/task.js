5f1633487f7c674e86487f6af3b12c9064f2ee32
YuliyaS <yuliyamarketplace@yandex.ru>
2025-02-01 18:20:43 +0400

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -5,7 +5,7 @@ function getArrayParams(...arr) {
 	let max = Infinity;
 	let sum = 0;
 
-	for (let i = 0; i < arr.lenght; i++) {
+	for (let i = 0; i < arr.length; i++) {
 		if (arr[i > max]) {
 			max = arr[i];
 		}
